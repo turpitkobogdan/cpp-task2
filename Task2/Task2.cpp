b1c35d6844d2b02c783009b12102560c37373dd6
bohdan <turpitkobogdan@gmail.com>
2020-03-13 10:24:02 +0200

    Добавьте файлы проекта.

---
+++ Task2/Task2.cpp
@@ -0,0 +1,38 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string s;
+static int num = 1;
+
+void gen(unsigned k)
+{
+	if (k == s.length())
+	{
+		cout << num++ << ":";
+		cout << s << '\n';
+		cout << endl;
+	}
+
+	else
+		for (unsigned j = k; j < s.length(); j++)
+			if (s[j] != s[j + 1])
+			{
+				swap(s[k], s[j]);
+				gen(k + 1);
+				swap(s[k], s[j]);
+			
+			}
+}
+
+int main()
+{	 
+	cout << "Input values:" << '\n';
+	cin >> s;
+	sort(s.begin(), s.end());
+	gen(0);
+	system("pause");
+	return 0;
+}
